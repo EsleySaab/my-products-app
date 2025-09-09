e089d52471cfaa65e39ab1abc9a432d13d166b95
EsleySaab <esleysantana.sa@gmail.com>
2025-09-09 20:06:48 -0300

    feat: adicionando botão de logout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,66 +1,47 @@
+"use client"
+
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
   NavbarBrand,
   NavbarItem,
-  NavbarMenuItem,
 } from "@heroui/navbar"
-import { Link } from "@heroui/link"
-import NextLink from "next/link"
-
-import { siteConfig } from "@/config/site"
 import { ThemeSwitch } from "@/components/theme-switch"
+import { useAuthStore } from "@/stores/authStore"
+import { Button } from "@heroui/button"
+import NextLink from "next/link"
+import { useRouter } from "next/navigation"
 
 export const Navbar = () => {
+  const { token, logout } = useAuthStore()
+  const router = useRouter()
+
+  const handleLogout = () => {
+    logout()
+    router.push("/login")
+  }
+
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+    <HeroUINavbar maxWidth="xl" position="sticky" className="px-4">
+      <NavbarContent justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink
-            className="flex justify-start items-center gap-1"
-            href="/"
-          ></NextLink>
+          <NextLink href="/" className="text-xl font-bold">
+            MyProductsApp
+          </NextLink>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden md:flex"></NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+      <NavbarContent justify="end" className="gap-2">
         <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
 
-      <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
+        {token ? (
+          <NavbarItem>
+            <Button size="sm" onClick={handleLogout}>
+              Logout
+            </Button>
+          </NavbarItem>
+        ) : null}
+      </NavbarContent>
     </HeroUINavbar>
   )
 }
